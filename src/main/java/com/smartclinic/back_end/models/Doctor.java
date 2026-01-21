08a1ca2c21fc9fd87ea5de093a7ef2d68cc3a292
Anuj Sharma <anujsharma11746@gmail.com>
2026-01-21 22:48:59 +0530

    Create Doctor entity with attributes and available times

---
+++ src/main/java/com/smartclinic/back_end/models/Doctor.java
@@ -0,0 +1,20 @@
+package com.smartclinic.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String specialty;
+
+    @ElementCollection
+    private List<LocalTime> availableTimes;
+}
